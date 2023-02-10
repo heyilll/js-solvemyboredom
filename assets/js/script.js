3f245275fa27cd1a7e91600b4de6c30879fe6554
Heil Park <heilpark@me.com>
2023-02-10 14:13:38 +0000

    linking js to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,61 @@
-// testing section
-// var choice = "";
-// var custom = "participants=3"
-
-// var queryURL = "https://www.boredapi.com/api/activity?" + custom;
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(response => extractInfo(response));
+// tracks activity history
+var pastactivities = [];
 
 // to be integrated with html
 $("#search-button").on("click", function(event) {
     event.preventDefault();
 
-    var search = $("#search-input").val().trim();
-    if (search == "") {
-        return;
-    }
+    var searchNum = $("#number-input :selected").text();
+    var searchType = $("#type-input :selected").text();
+    var queryURL = "https://www.boredapi.com/api/activity"
 
-    if (!search) {
-        choice = "/";
+    if (!searchNum && !searchType) {
+        queryURL = "https://www.boredapi.com/api/activity/";
+    } else if (searchNum && !searchType) {
+        queryURL = `https://www.boredapi.com/api/activity?participants=${searchNum}`;
+    } else if (!searchNum && searchType) {
+        queryURL = `https://www.boredapi.com/api/activity?type=${searchType}`;
     } else {
-        choice = "?";
+        queryURL = `https://www.boredapi.com/api/activity?type=${searchType}&participants=${searchNum}`;
     }
-
-    var queryURL = "https://www.boredapi.com/api/activity" + choice + search;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(response => extractInfo(response));
+
+    searchNum = $("#number-input").val("1");
+    searchType = $("#type-input").val("1");
 });
+
+$("#random-button").on("click", function(event) {
+    event.preventDefault();
+
+    var queryURL = "https://www.boredapi.com/api/activity";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(response => extractInfo(response));
+});    
 
 // extracts info about activity 
 function extractInfo({activity, link, price}) {
-    console.log(activity);
-    console.log(link);
-    console.log(price);
+    $("#activity-title").text(activity);
+    $("#activity-price").text(price);
+    $("#activity-link").text(link);
+
+    pastactivities.unshift(activity);
+
+    // working on saving to local storage
+    // localStorage.getItem(activity, JSON.stringify(link,price));
+
     youTubeSearch (activity);
 }
 
 // function to search for the youtube link given the text input from boredapi
 function youTubeSearch (text){
-       
+
     const settings = {
         "async": true,
         "crossDomain": true,
@@ -55,12 +68,11 @@ function youTubeSearch (text){
     };
     
     $.ajax(settings).done(function (response) {
-        // console.log(response);
-        // console.log(response.items[0].url);
         var youTube_link = response.items[0].url;
         var embeded_link = youTube_link.replace("watch?v=","embed/");
+
         // link to the html element to add src
         console.log(embeded_link);
-            
+        $("#videoembed").attr("src", embeded_link);
     });
 }
